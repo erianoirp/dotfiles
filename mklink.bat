16e329aff836cfc0e43c7bda33b99f0fb3587b87
Modevian <prilt5ogy@gmail.com>
2018-04-17 12:51:28 +0900

    anything

---
+++ mklink.bat
@@ -0,0 +1,4 @@
+mklink %HOMEPATH%\.vimrc %HOMEPATH%\dotfiles\.vimrc
+mklink /D %HOMEPATH%\.vim\colors %HOMEPATH%\dotfiles\colors
+mklink %HOMEPATH%\_gvimrc %HOMEPATH%\dotfiles\.gvimrc
+mklink /D %HOMEPATH%\vimfiles\colors %HOMEPATH%\dotfiles\colors
