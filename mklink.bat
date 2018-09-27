e39886e70a3d9ecbe1ec665b0f8e33fe0ad29983
omotonama <prilt5ogy@gmail.com>
2018-09-28 00:37:59 +0900

    pauseを追加

--- mklink.bat
+++ mklink.bat
@@ -3,3 +3,4 @@ mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
 mklink %HOMEPATH%\_gvimrc %HOMEPATH%\dotfiles\.gvimrc
 mklink /D %HOMEPATH%\.vim\colors %HOMEPATH%\dotfiles\colors
 mklink /D %HOMEPATH%\vimfiles\colors %HOMEPATH%\dotfiles\colors
+@pause
