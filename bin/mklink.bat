381ff1b38d86dea7dde40da26801d51590d3c4d2
erianoirp <prilt5ogy@gmail.com>
2019-10-14 19:36:48 +0900

    [Update] GitBash上でもdein.vimを使えるように修正

--- bin/mklink.bat
+++ bin/mklink.bat
@@ -4,7 +4,8 @@ mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\_vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\_gvimrc %HOME%\dotfiles\.gvimrc
 @rem for vim
-mkdir %HOME%\.vim\temp
+mkdir %HOME%\.vim\temp %HOME%\.vim\rc
+mklink %HOME%\.vim\rc\dein.toml %HOME%\dotfiles\rc\dein.toml
 mklink /D %HOME%\.vim\colors %HOME%\dotfiles\colors
 @rem for gvim
 mkdir %HOME%\vimfiles
